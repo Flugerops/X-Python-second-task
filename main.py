feafea0ab6494a62cbf2ca5c97a0a683662fc256
Bohdan Savin <bogdan07112009@gmail.com>
2024-07-28 17:13:11 +0300

    first version

---
+++ main.py
@@ -0,0 +1,13 @@
+def fix_ingredients(ingredients:list) -> list[str]:
+    if not isinstance(ingredients, list):
+        raise TypeError("ingredients must be list")
+    if not all(isinstance(item, str) for item in ingredients):
+        raise TypeError("All elements must be str")
+    for item in ingredients:
+        if not item.startswith("z"):
+            ingredients[ingredients.index(item)] = item[::-1]
+    return ingredients
+
+
+if __name__ == "__main__":
+    print(fix_ingredients(["sugar", "zmilk", "retaW", "salt", "honey"]))
